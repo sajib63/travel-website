e679b4b2c511dfa1ab4d9161efd79f20c4c54ca7
Sajib Ahmed <sajib7315@gmail.com>
2022-10-23 12:25:01 +0600

    added

--- src/Components/NewsDetails/Details.js
+++ src/Components/NewsDetails/Details.js
@@ -6,7 +6,7 @@ const Details = ({data}) => {
         <div className="card w-96 bg-base-100 shadow-xl flex">
         <figure><img src={data.image_url} alt="Shoes" /></figure>
         <div className="card-body">
-          <h2 className="card-title">Shoes!</h2>
+          <h2 className="card-title">{data.name}</h2>
           <p>If a dog chews shoes whose shoes does he choose?</p>
           <div className="card-actions justify-end">
             <button className="btn btn-primary">Buy Now</button>
